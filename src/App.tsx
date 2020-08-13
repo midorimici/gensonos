f46473d79546be31b0efe15c9c8936215bf7bb68
midori mici <midorimici.works@gmail.com>
2020-08-13 15:45:15 +0900

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import React from 'react';
 import Info from './components/Info'
 
 import './App.scss';
+import logo from './logo.svg';
 
 export default () => {
   return (
-    <div id='App'>
-			<section id='canvas-wrapper'>
-				<canvas id='graph'>
-					グラフを表示するには、canvasタグをサポートしたブラウザが必要です。
-				</canvas>
-			</section>
-			<Info />
-    </div>
+		<>
+			<img id='logo' src={logo} alt='logo' height={45} />
+			<div id='App'>
+				<section id='canvas-wrapper'>
+					<canvas id='graph'>
+						グラフを表示するには、canvasタグをサポートしたブラウザが必要です。
+					</canvas>
+				</section>
+				<Info />
+			</div>
+		</>
   );
 }
